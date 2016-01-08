dfd846b2be6ea60e2d45fdfd474d6cf82ce12870
hong qiu <qiuhong4@hotmail.com>
2016-01-07 18:23:01 -0800

    Initial Commit

---
+++ RIVA/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RIVA
+//
+//  Created by hong qiu on 2016-01-07.
+//  Copyright (c) 2016 Simon Fraser Univerity. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
